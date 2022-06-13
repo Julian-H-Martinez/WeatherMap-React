23a3c6ed05de0bc6963457969d18148e238ad7ef
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-06-13 17:33:37 -0500

    setup complete; need to refactor and style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+    const key = process.env.REACT_APP_API_KEY;
+    const lat = 29.424349;
+    const lon = -98.491142;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="location">
+            <h2>San Antonio</h2>
+        </div>
+        <div className="temp">
+            <h4>110°F</h4>
+        </div>
+        <div className="humidity">
+            85%
+        </div>
+
     </div>
   );
 }
