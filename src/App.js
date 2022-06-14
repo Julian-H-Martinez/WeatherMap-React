96d5a90039dda9eae6f15d0c98af3f02d94bbe8d
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-06-14 16:18:11 -0500

    have access to api; added assets folder for background image

--- src/App.js
+++ src/App.js
@@ -7,44 +7,41 @@ function App() {
     const key = process.env.REACT_APP_API_KEY;
     const lat = 29.424349;
     const lon = -98.491142;
-    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`
+    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units`
 
   return (
     <div className="app">
         <div className="container">
-            <h2>San Antonio</h2>
+            <div className="top">
+                <div className="location">
+                    <h2>San Antonio</h2>
+                </div>
+                <div className="temp">
+                    <h4>98.5°F</h4>
+                </div>
+                <div className="description">
+                    <h5>Clouds</h5>
+                </div>
+            </div>
+            <div className="bottom">
+                <div className="feels">
+                    <h5>110°F</h5>
+                </div>
+                <div className="humidity">
+                    <h5>85%</h5>
+                </div>
+                <div className="wind">
+                    <h5>2mph</h5>
+                </div>
+            </div>
         </div>
-        <div className="temp">
-            <h4>110°F</h4>
-        </div>
-        <div className="humidity">
-            85%
-        </div>
-
-
-        <Form className="container">
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
-
     </div>
   );
 }
 
 export default App;
+
+
+/*
+https://api.openweathermap.org/data/2.5/forecast?lat=29.424349&lon=-98.491142&appid=e36460df7ac92f34e5766229d1a0601e
+ */                                  
