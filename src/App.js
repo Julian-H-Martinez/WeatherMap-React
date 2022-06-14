ef46c6864f4952adf7dd8ac0ab7b4cff4cf88be9
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-06-14 17:42:26 -0500

    app is functional but still needs some solid styling/designing; upon return will continue styling the app and making it fully complete.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,70 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 
 function App() {
+    const [data, setData] = useState({});
+    const [location, setLocation] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
     const key = process.env.REACT_APP_API_KEY;
     const lat = 29.424349;
     const lon = -98.491142;
-    const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units`
+
+    const urlGeo = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
+    const urlCity = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=imperial`;
+    const urlCityState = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${key}&units=imperial`
+
+    const searchLocation = (e) => {
+        if(e.key === 'Enter'){
+            axios.get(urlCity)
+                .then((response) => {
+                    setData(response.data);
+                    setCity('');
+                    console.log(response.data);
+                })
+        }
+    }
 
   return (
     <div className="app">
+        <div className="search">
+            <input
+                type="text"
+                value={city}
+                onChange={event => setCity(event.target.value)}
+                onKeyPress={searchLocation}
+                placeholder='Enter City'
+            />
+        </div>
         <div className="container">
             <div className="top">
                 <div className="location">
-                    <h2>San Antonio</h2>
+                    <h2>{data.name}</h2>
                 </div>
                 <div className="temp">
-                    <h4>98.5°F</h4>
+                    {data.main ? <h4>{data.main.temp.toFixed()}°F</h4> : null}
                 </div>
                 <div className="description">
-                    <h5>Clouds</h5>
+                    {data.weather ? <h5>{data.weather[0].main}</h5> : null}
                 </div>
             </div>
-            <div className="bottom">
-                <div className="feels">
-                    <p className="bold text">110°F</p>
-                    <p className='text'>Feels Like</p>
+
+            {data.name !== undefined &&
+                <div className="bottom">
+                    <div className="feels">
+                        <p className="bold text">110°F</p>
+                        <p className='text'>Feels Like</p>
+                    </div>
+                    <div className="humidity">
+                        <p className='bold text'>85%</p>
+                        <p className='text'>Humidity</p>
+                    </div>
+                    <div className="wind">
+                        <p className="bold text">2mph</p>
+                        <p className='text'>Wind Speed</p>
+                    </div>
                 </div>
-                <div className="humidity">
-                    <p className='bold text'>85%</p>
-                    <p className='text'>Humidity</p>
-                </div>
-                <div className="wind">
-                    <p className="bold text">2mph</p>
-                    <p className='text'>Wind Speed</p>
-                </div>
-            </div>
+            }
         </div>
     </div>
   );
